43cd70de570bedc03d3cab82e946a24e7f416dc0
Caio Nunes <caionuneso@hotmail.com>
2021-01-28 17:59:24 -0300

    AwsConfig

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,13 +1,28 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 import path from 'path';
 import crypto from 'crypto';
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
 export default {
   directory: tempFolder,
-  storage: multer.diskStorage({
+  /* storage: multer.diskStorage({
     destination: tempFolder,
     filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const filename = `${fileHash}-${file.originalname}`;
+
+      return callback(null, filename);
+    },
+  }), */
+
+  storage: multerS3({
+    s3: new aws.S3(),
+    bucket: 'denunciations-images',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (request, file, callback) => {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const filename = `${fileHash}-${file.originalname}`;
 
